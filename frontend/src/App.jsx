fa8cf4dcf0def1c74d1030cdaf0c3460a289e54b
Lucho Web <luchoweb.dev@gmail.com>
2022-10-05 13:24:10 -0500

    Modal done (#7)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,14 @@ import getMovies from './api/getMovies'
 import MovieCard from './components/MovieCard'
 import LoadMoreButton from './components/LoadMoreButton'
 import SearchMovie from './components/SearchMovie'
+import Modal from './components/Modal'
 
 import './styles/App.scss'
 
 function App() {
   const { state: movies, dispatch } = useMovies()
   const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -35,49 +36,58 @@ function App() {
   }, [])
 
   return (
-    <section className="movies">
-      <div className="container">
-        <h1 className="movies__title">
-          2020 "Love" Movies
-        </h1>
+    <>
+      {error && (
+        <Modal data={{
+          title: 'Aviso',
+          message: 'Error al obtener listado. Intente nuevamente.'
+        }} />
+      )}
 
-        <SearchMovie />
+      <section className="movies">
+        <div className="container">
+          <h1 className="movies__title">
+            2020 "Love" Movies
+          </h1>
 
-        <ul className="movies__list">
-          {
-            error ? (
-              <li className='movies__list-item--w100'>
-                Ha ocurrido un error. Por favor refresque la ventana.
-              </li>
-            )
-            
-            : !error && isLoading ? (
-              <li className='movies__list-item--w100'>
-                Cargando películas, por favor espere...
-              </li>
-            )
-            
-            : !isLoading && movies?.length ? movies.map(movie => (
-              <li className='movies__list-item' key={`k-${movie.id}-${movie.title}`}>
-                <MovieCard movie={movie} />
-              </li>
-            ))
+          <SearchMovie />
 
-            : (
-              <li className='movies__list-item--w100'>
-                No hay películas para mostrar.
-              </li>
-            )
-          }
-        </ul>
+          <ul className="movies__list">
+            {
+              error ? (
+                <li className='movies__list-item--w100'>
+                  Ha ocurrido un error. Por favor refresque la ventana.
+                </li>
+              )
+              
+              : !error && isLoading ? (
+                <li className='movies__list-item--w100'>
+                  Cargando películas, por favor espere...
+                </li>
+              )
+              
+              : !isLoading && movies?.length ? movies.map(movie => (
+                <li className='movies__list-item' key={`k-${movie.id}-${movie.title}`}>
+                  <MovieCard movie={movie} />
+                </li>
+              ))
 
-        { !isLoading && movies?.length >= 12 && (
-          <div className="movies__button-more">
-            <LoadMoreButton />
-          </div>
-        )}
-      </div>
-    </section>
+              : (
+                <li className='movies__list-item--w100'>
+                  No hay películas para mostrar.
+                </li>
+              )
+            }
+          </ul>
+
+          { !isLoading && !error && (
+            <div className="movies__button-more">
+              <LoadMoreButton />
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   )
 }
 
---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import '../styles/components/Modal.scss'
+
+const Modal = ({ data }) => {
+  const [show, setShow] = useState(true)
+
+  return show && (
+    <div className={`modal`}>
+      <div className="modal__body">
+        <h5 className="modal__title">
+          { data.title }
+        </h5>
+        <p className="modal__message">
+          { data.message }
+        </p>
+
+        <button
+          className="modal__button"
+          onClick={() => setShow(!show)}
+        >
+          Aceptar
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
