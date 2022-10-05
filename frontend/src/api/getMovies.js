cf8dc6ff3187bf14928ea80cd314868527ba3349
Lucho Web <luchoweb.dev@gmail.com>
2022-10-05 08:40:36 -0500

    limit param added to getMovies function

--- frontend/src/api/getMovies.js
+++ frontend/src/api/getMovies.js
@@ -1,6 +1,6 @@
-const getMovies = async (page) => {
+const getMovies = async (page = 1, limit = 12) => {
   const apiUrl = import.meta.env.VITE_API_MOVIES
-  const response = await fetch(`${apiUrl}?page=${page || 1}`)
+  const response = await fetch(`${apiUrl}?page=${page}&limit=${limit}`)
   const movies = await response.json()
 
   return movies
