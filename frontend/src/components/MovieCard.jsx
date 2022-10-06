9e082de60e02d48a5fe6f1c738751bd60b5e4b7a
Lucho Web <luchoweb.dev@gmail.com>
2022-10-06 09:06:57 -0500

    404 image error

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
+
 import '../styles/components/MovieCard.scss'
 
 export default function MovieCard({ movie }) {
   const noPoster = 'https://via.placeholder.com/200x300.png?text=No+poster'
   const moviePoster = movie.poster === 'N/A' ? noPoster : movie.poster
+
+  const [poster, setPoster] = useState(moviePoster)
 
   return (
     <div className="movie-card">
@@ -10,7 +14,11 @@ export default function MovieCard({ movie }) {
         className="movie-card__poster"
         data-testid="movie-poster"
       >
-        <img src={moviePoster} alt={movie.title} />
+        <img
+          src={poster}
+          alt={movie.title}
+          onError={() => setPoster(noPoster)}
+        />
       </figure>
 
       <div className="movie-card__details">
